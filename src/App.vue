f4dae2486c6ce6c8a9f1a9963f45bd768ee339dc
TurboCsiga <bkrisz.100@gmail.com>
2022-01-18 17:10:11 +0100

    kesz

--- src/App.vue
+++ src/App.vue
@@ -1,134 +1,33 @@
 <template>
   <div id="app">
+    <button v-on:click="isHidden = !isHidden">Painting</button>
+    <button v-on:click="isHidden2 = !isHidden2, isHidden = !isHidden">Mindkettő</button>
+    <button v-on:click="isHidden2 = !isHidden2">Statues</button>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Azonsító</th>
-          <th>Cím</th>
-          <th>Év</th>
-          <th>Kiállítva(piros lap)</th>
-          <th>Műveletek</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="painting in paintings" v-bind:key="painting.id">
-          <td>{{ painting.id }}</td>
-          <td>{{ painting.title }}</td>
-          <td>{{ painting.year }}</td>
-          <td>{{ painting.on_display }}</td>
-          <td>
-            <button @click="deletePainting(painting.id)">Törlés</button>
-            <button @click="editPainting(painting.id)">Szerkesztés</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <input type="hidden" v-model="painting.id">
-          </td>
-          <td>
-            <input type="text" v-model="painting.title">
-          </td>
-          <td>
-            <input type="number" v-model="painting.year">
-          </td>
-          <td>
-            <input type="checkbox" v-model="painting.on_display">
-          </td>
-          <td>
-            <button v-if="mod_new" @click="newPainting" :disabled="saving">Létrehoz</button>
-            <button v-if="!mod_new" @click="savePainting" :disabled="saving">Mentés</button>
-            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving">Mégse</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <button @click="loadData">Adatok betöltése</button>
+    <Painting v-if="!isHidden"></Painting>
+    <Statue v-if="!isHidden2"></Statue>
   </div>
 </template>
 
+
+
 <script>
+import Painting from './components/Painting.vue';
+import Statue from "./components/Statue.vue";
 
 export default {
   name: 'App',
+  
   components: {
+    Painting,
+    Statue
   },
-  data() {
-    return {
-      mod_new: true, 
-      saving: false,
-      painting: {
-        id: null,
-        title: '',
-        year: '',
-        on_display: false
-      },
-      paintings: []
+  data () {
+        return {
+            isHidden: false,
+            isHidden2: false
+        }
     }
-  },
-  methods: {
-    async loadData () {
-     let Response = await fetch('http://127.0.0.1:8000/api/paintings')
-     let data = await Response.json()
-     this.paintings = data
-    },
-    async deletePainting(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/paintings/${id}`, {
-        method: 'DELETE'
-      })
-      console.log(Response)
-      await this.loadData()
-    },
-    async newPainting() {
-      this.saving='disabled'
-     await fetch('http://127.0.0.1:8000/api/paintings', {
-       method: 'POST',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.painting) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async savePainting() {
-      this.saving='disabled'
-     await fetch(`http://127.0.0.1:8000/api/paintings/${this.painting.id}`, {
-       method: 'PATCH',
-       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       },
-       body: JSON.stringify(this.painting) 
-     })
-     await this.loadData()
-     this.saving=false
-     this.resetForm()
-    },
-    async editPainting(id) {
-      let Response = await fetch(`http://127.0.0.1:8000/api/paintings/${id}`)
-      let data = await Response.json()
-      this.painting = {...data};
-      this.mod_new = false
-    },
-    cancelEdit () {
-      this.resetForm()
-    },
-    resetForm() {
-      this.painting = {
-        id: null,
-        title: '',
-        year: '',
-        on_display: false
-      }
-      this.mod_new = true
-    }
-  },
-  mounted() {
-    this.loadData()
-  }
 }
 </script>
 
